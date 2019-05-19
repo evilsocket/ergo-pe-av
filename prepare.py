2056e1fc8a7b6a6262d71c051a8434d57d9d2d1c
evilsocket <evilsocket@gmail.com>
2019-05-19 19:23:29 +0200

    new: model autogenerates the attribute.names file for ergo relevance

--- prepare.py
+++ prepare.py
@@ -1,14 +1,24 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
-
+import logging as log
 import numpy as np
 import pandas as pd
 
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+
 import encoder
 
+def prepare_names():
+    path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "attribute.names")
+    log.info("creating %s" % path)
+    with open(path, 'w+t') as fp:
+        for name in encoder.attribute_names():
+            fp.write("%s\n" % name)
+
+
 def prepare_dataset(filename):
+    prepare_names()
     data = pd.read_csv(filename, sep = ',', header = None)
     return data.replace(encoder.classes)
 
