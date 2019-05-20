0b0a690788bd28d14584cf26684121dc89000341
evilsocket <evilsocket@gmail.com>
2019-05-20 13:28:43 +0200

    new: implemented checkpoints

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
+import os
 import logging as log
 
-from keras.callbacks import EarlyStopping
+from keras.callbacks import EarlyStopping, ModelCheckpoint
 
 # define training strategy
 def train_model(model, dataset):
@@ -14,10 +15,16 @@ def train_model(model, dataset):
     
     model.compile(loss = loss, optimizer = optimizer, metrics = metrics)
 
-    earlyStop = EarlyStopping(monitor = 'val_acc', min_delta=0.0001, patience = 5, mode = 'auto')
+    checkpath = os.path.join(os.path.dirname(os.path.abspath(__file__)), "checkpoints")
+    os.makedirs(checkpath, exist_ok=True)
+    checkpath = os.path.join(checkpath, 'model-epoch{epoch:03d}-acc{val_acc:03f}.h5')
+
+    stopper = EarlyStopping(monitor = 'val_acc', min_delta=0.0001, patience = 5, mode = 'auto')
+    saver   = ModelCheckpoint(checkpath, save_best_only=True, verbose=1, monitor='val_loss', mode='min')
+
     return model.fit( dataset.X_train, dataset.Y_train,
             batch_size = 64,
             epochs = 50,
             verbose = 2,
             validation_data = (dataset.X_val, dataset.Y_val),
-            callbacks = [earlyStop])
+            callbacks = [saver, stopper])
