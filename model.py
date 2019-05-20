77f3ab3b72c8b372690361dc359cbab7026c5a2c
evilsocket <evilsocket@gmail.com>
2019-05-20 13:33:12 +0200

    misc: simpler model declaration

--- model.py
+++ model.py
@@ -1,28 +1,23 @@
 import logging as log
 
 from keras.models import Sequential
-from keras.layers import Dense, Activation, Dropout
+from keras.layers import Dense, Dropout
 
 def build_model(is_train):  
-    n_inputs       = 486
-    n_hidden       = (70, 70,)
-    dropout        = 0.3
-    activation     = 'relu'
-    out_activation = 'softmax'
-  
+    n_inputs = 486
     log.info("building model for %s ..." % 'training' if is_train else 'evaluation')
-
-    model = Sequential()
-    for i, n_neurons in enumerate(n_hidden):
-        # setup the input layer
-        if i == 0:
-            model.add(Dense(n_neurons, input_shape = (n_inputs,), activation = activation))
-        else:
-            model.add(Dense(n_neurons, activation = activation))
-        # add dropout
-        if is_train:
-            model.add(Dropout(dropout))
-    # setup output layer
-    model.add(Dense(2, activation = out_activation))
-    
-    return model
+    # only add dropout for training
+    if is_train:
+        return Sequential([
+            Dense(70, input_shape=(n_inputs,), activation='relu'),
+            Dropout(0.3),
+            Dense(70, activation='relu'),
+            Dropout(0.3),
+            Dense(2, activation='softmax')
+        ])
+    else:
+        return Sequential([
+            Dense(70, input_shape=(n_inputs,), activation='relu'),
+            Dense(70, activation='relu'),
+            Dense(2, activation='softmax')
+        ])
