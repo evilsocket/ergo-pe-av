015e1b2f410719c8f09fcc13f84da7ad5a201171
evilsocket <evilsocket@gmail.com>
2019-05-20 18:08:47 +0200

    misc: small fix or general refactoring i did not bother commenting

--- model.py
+++ model.py
@@ -3,10 +3,13 @@ import logging as log
 from keras.models import Sequential
 from keras.layers import Dense, Dropout
 
+# used by `ergo train <path>` to build the model structure
 def build_model(is_train):  
+    # our model is a simple ANN with 486 inputs, two hidden
+    # layers of 70 neurons each, dropout of 30% while training
+    # and two output classes (0=clean 1=malicious)
     n_inputs = 486
     log.info("building model for %s ..." % 'training' if is_train else 'evaluation')
-    # only add dropout for training
     if is_train:
         return Sequential([
             Dense(70, input_shape=(n_inputs,), activation='relu'),
--- prepare.py
+++ prepare.py
@@ -10,6 +10,7 @@ sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
 import encoder
 
+# generate an attribute.names file to be used with `ergo relevance <path> --attributes attributes.names`
 def prepare_names():
     path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "attribute.names")
     log.info("creating %s" % path)
@@ -17,12 +18,14 @@ def prepare_names():
         for name in encoder.attribute_names():
             fp.write("%s\n" % name)
 
-
+# called by `ergo train <path> --dataset file.csv` to parse the dataset.
 def prepare_dataset(filename):
     prepare_names()
     data = pd.read_csv(filename, sep = ',', header = None)
     return data.replace(encoder.classes)
 
+# used by `ergo encode <path> <folder>` to encode a PE in a vector of scalar features
+# used by `ergo serve <path>` to parse the input query before running the inference
 def prepare_input(x, is_encoding = False):
     # file upload
     if isinstance(x, werkzeug.datastructures.FileStorage):
